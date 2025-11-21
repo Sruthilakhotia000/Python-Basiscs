1e020dd8bbed7b29af2331566dbe4a388862b642
Sruthilakhotia000 <sruthilakhotia@gmail.com>
2025-11-21 12:27:47 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,51 @@
+# print("Hello World!")
+# a=10
+# b=50.6
+# print("Addition:",a+b)
+# print("Subtraction:",a-b)
+# print("Multiplication:",a*b)
+# print("Division:",a/b)
+# print("Modulo:",a%b)
+# d="Web Development"
+# print(a)
+# print(type(a))
+# print(type(b))
+# print(type(d))
+
+# p = int(input("Enter first number: "))
+# q = int(input("Enter second number: "))
+# r=p+q
+# print("Addition:",r)
+
+
+# kg = int(input("Enter weight in kg: "))
+# pounds = kg * 2.20462
+# print("Weight in pounds:", pounds)
+
+# km = int(input("Enter distance in km: "))
+# miles = km * 0.621371
+# print("Distance in miles:", miles)
+
+# principal = float(input("Enter principal amount: "))
+# rate = float(input("Enter rate of interest: "))
+# time = float(input("Enter time in years: "))
+
+# simple_interest = (principal * rate * time) / 100
+# print("Simple Interest:", simple_interest)
+
+s="python"
+print(s[-3])
+print(len(s))
+print(s +" Programming")
+print(s[0:3])
+print(s*3)
+print(s[::-1]) #reverse string
+print(s.upper()) #uppercase
+print(s.lower()) #lowercase
+print(s.replace("y","o"))
+print(s.split("h")) #it will split in words from h into two different words
+
+
+
+
+
